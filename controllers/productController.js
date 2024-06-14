7c15fd06020d97b0f2755d8130c772b28c9bd9fe
Bikash Shaw <1985.bikash.shaw@gmail.com>
2024-06-14 11:28:42 +0530

    Sequelize implemented

--- controllers/productController.js
+++ controllers/productController.js
@@ -76,19 +76,41 @@ exports.renderEditProduct = async (req, res) => {
   }
 };
 
-exports.editProduct = (req, res) => {
-  const { productname, price } = req.body;
-  const img = req.file.destination + "/" + req.file.filename;
-  const id = req.params.id;
+exports.editProduct = async (req, res) => {
+  try {
+    const { productname, price } = req.body;
+    const img = req.file.destination + "/" + req.file.filename;
+    const id = req.params.id;
 
-  const products = new Products(id, productname, price, img);
-  products.editData().then(() => {
+    const product = await Product.findByPk(id);
+    if (!product) {
+      console.error("Product not found !");
+      return res.status(404).send("Product not found !");
+    }
+    product.productname = productname;
+    product.price = price;
+    product.img = img;
+
+    await product.save();
+    console.log("Product edited successfully");
     res.redirect("/");
-  });
+  } catch (err) {
+    console.error(err);
+    res.status(500).redirect("/error");
+  }
 };
 
-exports.deleteProduct = (req, res) => {
-  Products.deleteProductById(req.params.id).then(() => {
+exports.deleteProduct = async (req, res) => {
+  try {
+    await Product.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    console.log("Product deleted successfully");
     res.redirect("/");
-  });
+  } catch (err) {
+    console.error(err);
+    res.status(500).redirect("/error");
+  }
 };
